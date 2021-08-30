3e6912be8e99b703efa4c97661c2d50658e76a43
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-08-30 23:28:27 +0200

    :construction: First step

--- AboutThisComputer.sh
+++ AboutThisComputer.sh
@@ -2,6 +2,13 @@
 
 echo "\x1B[33m bonjour je vais bien en jaune\x0A\x1B[34m et maintenant en bleu\x0A\x1B[35m\x1B[102mtest\x1B[0m";
 
-$I "Toto"
+YELLOW="\x1B[33m"
+RESET="\x1B[0m"
+BLUE="\x1B[34m"
+I="Toto"
 
-echo $I
+UNAMEALL=$(uname -a)
+
+echo $YELLOW $I $RESET
+
+echo $YELLOW $UNAMEALL $RESET
